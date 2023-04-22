a33ed93addf9a505bb232e0571faca2fc20dd14d
hahasuperhero <wiiwn2728@gmail.com>
2023-04-22 22:41:21 +0800

    實作自動搜尋yahoo排行榜的爬蟲 並且會重置資料庫的id 以利id=排名的方式進行使用

--- src/main/java/team3/meowie/crawler/controller/crawlerController.java
+++ src/main/java/team3/meowie/crawler/controller/crawlerController.java
@@ -1,10 +1,14 @@
 package team3.meowie.crawler.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import team3.meowie.crawler.model.TopMovielist;
 import team3.meowie.crawler.service.CommentsService;
 import team3.meowie.crawler.service.MovielistService;
 import team3.meowie.crawler.service.TopMovielistService;
@@ -17,9 +21,17 @@ public class crawlerController {
 	private MovielistService movielistService;
 	@Autowired
 	private TopMovielistService topMovielistService;
+
 	@GetMapping("crawler/comment/yahoo")
 	public String autoCrawlerCommentsFromYahoo(@RequestParam(name = "moviename") String moviename) {
 		commentsService.autoCrawlerCommentsFromYahoo(moviename);
 		return "crawler/test";
 	}
+
+	@ResponseBody
+	@GetMapping("crawler/toplist/yahoo")
+	public  List<TopMovielist> autoCrawlerTopMovielistFromYahoo(){
+		return topMovielistService.autoCrawlerTopMovielistFromYahoo();
+		}
+
 }
