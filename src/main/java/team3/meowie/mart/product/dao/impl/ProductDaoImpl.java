011c403fb3c181eb7b8e6aedb39d67db30dfc1f6
PIGERGOD <pigergod@hotmail.com>
2023-04-24 18:43:47 +0800

    建好目前需要的package跟檔案

---
+++ src/main/java/team3/meowie/mart/product/dao/impl/ProductDaoImpl.java
@@ -0,0 +1,9 @@
+package team3.meowie.mart.product.dao.impl;
+
+/**
+ * ClassName:ProductDaoImpl
+ * Description:
+ * Create:2023/4/24 下午 06:40
+ */
+public class ProductDaoImpl {
+}
---
+++ src/main/java/team3/meowie/mart/product/service/impl/ProductServiceImpl.java
@@ -0,0 +1,9 @@
+package team3.meowie.mart.product.service.impl;
+
+/**
+ * ClassName:ProductServiceImpl
+ * Description:
+ * Create:2023/4/24 下午 06:39
+ */
+public class ProductServiceImpl {
+}
